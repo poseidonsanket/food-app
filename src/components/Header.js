4e3cba44b6c3f22a5d4490f487b7a0a6b2f80769
poseidonsanket <sanketdadali05@gmail.com>
2024-02-05 19:40:09 +0530

    links added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
   const [btnName, setBtnName] = useState("Login");
@@ -11,9 +12,9 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About Us</Link></li>
+          <li><Link to="/contact">Contact Us</Link></li>
           <li>Cart</li>
           <button
             className="login-btn"
