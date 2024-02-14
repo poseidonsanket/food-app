538e12f8cde353943d096cae17707d21e820fa8a
poseidonsanket <sanketdadali05@gmail.com>
2024-02-14 17:13:44 +0530

    css fixed

--- src/components/User.js
+++ src/components/User.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const User = (props) => {
   return (
-    <div className="m-1 p-4 border border-solid border-black border-4">
+    <div className="m-1 p-4 border-solid border-black border-4">
       <h1>Function Based Component</h1>
       <h2>Name: {props.name}</h2>
       <h3>Location: Pune</h3>
